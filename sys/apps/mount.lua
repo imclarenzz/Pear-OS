edc00edf77f3d0c454d5491e444e42d9adb725f6
imclarenz <clarenzmendiola33@gmail.com>
2024-11-02 18:04:47 -0300

    Delete sys directory

--- sys/apps/mount.lua
+++
@@ -1,6 +0,0 @@
-local args = { ... }
-
-local target = table.remove(args, 1)
-target = shell.resolve(target)
-
-fs.mount(target, table.unpack(args))
--- sys/modules/opus/ui/components/MenuItem.lua
+++
@@ -1,9 +0,0 @@
-local class = require('opus.class')
-local UI    = require('opus.ui')
-
-UI.MenuItem = class(UI.FlatButton)
-UI.MenuItem.defaults = {
-	UIElement = 'MenuItem',
-	noPadding = false,
-	textInactiveColor = 'gray',
-}
